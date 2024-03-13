fef7cc8426c235eeb03da61c75506bb90e62be69
fisher <fisher_thu@foxmail.com>
2024-03-13 12:50:43 +0000

    optimize prompt

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
+# 需要挂代理
+
+export http_proxy=http://127.0.0.1:1087
+export https_proxy=http://127.0.0.1:1087
+
+# 启动prompt flow demo
 /root/miniconda3/envs/langchain/bin/python -m promptflow._cli._pf.entry flow serve --source /root/chat_agent --port 18888 --host 0.0.0.0 --user-agent "prompt-flow-extension/1.14.0 (linux; x64) VSCode/1.87.0"
