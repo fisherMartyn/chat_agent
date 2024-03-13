3ae6d50256920350ce1423456669c1b3c5baf101
fisher <fisher_thu@foxmail.com>
2024-03-13 02:09:28 +0000

    first version

---
+++ start.sh
@@ -0,0 +1,1 @@
+/root/miniconda3/envs/langchain/bin/python -m promptflow._cli._pf.entry flow serve --source /root/chat_agent --port 18888 --host 0.0.0.0 --user-agent "prompt-flow-extension/1.14.0 (linux; x64) VSCode/1.87.0"
